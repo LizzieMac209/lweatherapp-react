2b999fade634ff4a53349de28476c37eb6bd2270
LizzieMac209 <Emacfarlane05@gmail.com>
2023-09-06 14:04:26 -0700

    Inserted Weather, WeatherInfo, Forecast, Formatted Day structure in js

---
+++ src/WeatherForecast.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import ForecastDay from "./ForecastDay";
+
+export default function WeatherForecast(props) {
+  let [ loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
+  useEffect(()  => {
+    setLoaded(false);
+}, [props.city]);
+
+function handleForecast(response){
+  setLoaded(true);
+  setForecast(response.data.daily);
+}
+function load (){
+   const apiKey = "23f35a041todc2b80f91930b959fa171";
+   const apiForecastUrl = `https://api.shecodes.io/weather/v1/current?query=${props.city}&key=${apiKey}&units=metric`;
+   axios.get(apiForecastUrl).then(handleForecast);
+}
+
+if (loaded) {
+  return (
+    <div className="WeatherForecast">
+      <div className="row">
+        {forecast.map(function (dailyForecast, index) {
+          if (index < 5) {
+            return (
+              <div className="col forecast" key={index}>
+                <ForecastDay data={dailyForecast} />
+                </div>
+            );
+          } else {
+            return null;
+          }
+        })}
+      </div>
+    </div>
+  );
+} else {
+  load();
+}
+}
