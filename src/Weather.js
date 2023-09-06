05dae0d4fa677b0b6a778941a6a8498cfb540bb0
LizzieMac209 <Emacfarlane05@gmail.com>
2023-09-06 12:16:11 -0700

    Added Weather js

--- src/Weather.js
+++ src/Weather.js
@@ -1,11 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
-export default function Weather(){
+
+export default function Weather(props){
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
+
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      city: response.data.city,
+      date: new Date(response.data.time * 1000),
+      temperature:response.data.temperature.current,
+      humidity: response.data.temperature.humidity,
+      conditions: response.data.condition.description,
+      feelsLike: response.data.temperature.feels_like,
+      wind: response.data.wind.speed,
+      icon:response.data.condition.icon_url,
+    });
+  }
+  function search(){
+    const apiKey = "23f35a041todc2b80f91930b959fa171";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+function handleSubmit(event){
+  event.preventDefault();
+  search();
+}
+function handleCity(event) {
+  setCity(event.target.value);
+}
+if (weatherData.ready) {
   return (
-    <h2>
-      Hello from Weather js
-    </h2>
-  )
+    <div classnName="Weather">
+      <form onSubmit={handleSubmit}>
+<div className="row m-2">
+<div className="col-9 p-0">
+<input
+type="search"
+placeholder="Enter city..."
+autoFocus
+className="form-control"
+onChange={handleCity}
+/>
+</div>
+<div classnName="col-3 pe-0">
+  <input type="submit" value="Search" className="btn btn-primary" />
+</div>
+</div>
+      </form>
+      <WeatherInfo data={weatherData} />
+      <WeatherForecast city={weatherData.city} />
+      <h2>Hello from Weather js</h2>
+    </div>
+  );
+} else {
+  search();
+  return "Loading...";
+}
 }
 
 
